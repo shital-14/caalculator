5e04379d3fa840e8c1ac4f17380332a48f18b8b8
shital-14 <143473476+shital-14@users.noreply.github.com>
2023-09-28 18:49:26 +0530

    Add files via upload

---
+++ tiktok.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	cout<< "\n\n  T i c k  C r o s s  G a m e"<<endl;
+	cout<<"\tPlayer1 [x] \n\tplayer2 [0]\n\n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t   1 |   2 |  3  \n";
+	cout<<"\t\t-----|-----|-----\n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t  4  |   5 |   6 \n";
+	cout<<"\t\t-----|-----|-----\n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\t\t  7  |  8  |  9  \n";
+	cout<<"\t\t     |     |     \n";
+	cout<<"\n\tPlayer1 [x] turn:";
+	
+	
+}
